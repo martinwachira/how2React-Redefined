35e1f7c23e17e3d7b7e9b12f745289b0b46ce8dd
martin-wachira <martinnwachira@gmail.com>
2021-05-05 22:18:10 +0300

    intro props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 2,
+      date: new Date(2019, 3, 14),
+      title: "House Door",
+      price: 110,
+    },
+    {
+      id: 2,
+      date: new Date(2017, 5, 20),
+      title: "Laptop",
+      price: 120,
+    },
+    {
+      id: 3,
+      date: new Date(2021, 11, 30),
+      title: "Car Engine",
+      price: 8900,
+    },
+    {
+      id: 4,
+      date: new Date(2015, 7, 30),
+      title: "Heater",
+      price: 150,
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2020, 3, 24);
-  const expenseTitle = "House Insurance";
-  const expensePrice = 456.98;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
