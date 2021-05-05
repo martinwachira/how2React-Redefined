101643557e21ad955cc5d8d350d70c331c296767
martin-wachira <martinnwachira@gmail.com>
2021-05-05 21:25:13 +0300

    remove hardcoded values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2020, 3, 24);
+  const expenseTitle = "House Insurance";
+  const expensePrice = 456.98;
+
   return (
     <div className="expense-item">
-      <div>March 20th 2020</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>House Insurance</h2>
-        <div className="expense-item__price">$340.85</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
