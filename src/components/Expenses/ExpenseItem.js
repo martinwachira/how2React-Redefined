1759128be23c515f2de6a094a387419ade9953c1
martin-wachira <martinnwachira@gmail.com>
2021-05-18 12:33:40 +0300

    ...intro states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // intro states
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    console.log("title change");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate dat={props.date} />
@@ -11,8 +18,9 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
