36af59eb449b23ea96af71c3dbbbff49b90592b4
martin-wachira <martinnwachira@gmail.com>
2021-06-02 19:33:43 +0300

    ...render list of data dynamically remove hardcoded values of comp ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // intro states
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    console.log("title change");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate dat={props.date} />
@@ -18,7 +11,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change title</button> */}
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,26 +18,13 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeHandler={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.item[0].title}
-          price={props.item[0].price}
-          date={props.item[0].date}
-        />
-        <ExpenseItem
-          title={props.item[1].title}
-          price={props.item[1].price}
-          date={props.item[1].date}
-        />
-        <ExpenseItem
-          title={props.item[2].title}
-          price={props.item[2].price}
-          date={props.item[2].date}
-        />
-        <ExpenseItem
-          title={props.item[3].title}
-          price={props.item[3].price}
-          date={props.item[3].date}
-        />
+        {props.item.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
