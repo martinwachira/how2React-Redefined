3823247a6302dfdd8f9bb14dcdb25735ae644034
martin-wachira <martinnwachira@gmail.com>
2021-06-03 21:35:19 +0300

    ...modify

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,12 +5,12 @@ import "./NewExpense.css";
 const NewExpense = (props) => {
   const [open, setOpen] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
-    setOpen(false);
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setOpen(false);
   };
 
   const handleOpen = () => {
@@ -21,21 +21,10 @@ const NewExpense = (props) => {
     setOpen(false);
   };
 
-  // if (open === true) {
-  //   return (
-  //     <div className="new-expense">
-  //       <ExpenseForm
-  //         onCancel={onCloseForm}
-  //         onSaveExpenseData={saveExpenseDataHandler}
-  //       />
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="new-expense">
-      {open === false && <button onClick={handleOpen}>Add New Expense</button>}
-      {open === true && (
+      {!open && <button onClick={handleOpen}>Add New Expense</button>}
+      {open && (
         <ExpenseForm
           onCancel={onCloseForm}
           onSaveExpenseData={saveExpenseDataHandler}
